e125f23bf9d6f95cf2d03d48fc356abe261dc49b
ysh1th <98638840+ysh1th@users.noreply.github.com>
2022-12-02 12:45:21 +0400

    Update asgn.java
    inserted toLowerCase for district selected by the user

--- asgn.java
+++ asgn.java
@@ -1,7 +1,7 @@
 
 import java.util.*;
 
-public class asgn {
+public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("welcome to expo2020");
@@ -15,7 +15,7 @@ public class asgn {
         int i;
         for(i=1;i<=n;i++) {
             System.out.println("enter name"+i);
-            String def = s.nextLine();
+            String garbageScan1 = s.nextLine();
             Name[i]=s.nextLine();
             System.out.println(Name[i]);
             System.out.println("enter age");
@@ -33,10 +33,18 @@ public class asgn {
         // System.out.println("the population @expo2020: "+population.getPopulation(month, date, hour));
 
         System.out.println("which district would you like to enter first? ");
-        String dist = s.nextLine();
-
         district d= new district();
         d.dists();
+        String garbageScan1 = s.nextLine();
+        String selectDist = s.nextLine();
+        String currentDist = selectDist.toLowerCase();
+        System.out.println(currentDist);
+
+        //call for method in a class to check if currentDist == district
+        // and call for function in that function to display the pavilions
+
+
+
         //use iterator to display districts stored in linkedlists, which is to be initiated in another class
 
         // if district sustainability: parking A-c
