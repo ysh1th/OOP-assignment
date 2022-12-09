2d768539bd3d2321a8f2cf443d6561860efb6ff4
ysh1th <98638840+ysh1th@users.noreply.github.com>
2022-12-09 18:00:34 +0400

    Update asgn.java
    new update

--- asgn.java
+++ asgn.java
@@ -1,56 +1,95 @@
-
 import java.util.*;
+import distpav.*;
 
-public class Main {
+public class asgn {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("welcome to expo2020");
-        // enter user details
+        System.out.println("welcome to expos0s0");
+
         System.out.println("Pls enter number of ppl to enter");
-        int n, age;
-        String name;
-        n = s.nextInt();
-        String[] Name = new String[n+1];
-        int[] Age = new int[n+1];
-        int i;
-        for(i=1;i<=n;i++) {
-            System.out.println("enter name"+i);
-            String garbageScan1 = s.nextLine();
-            Name[i]=s.nextLine();
-            System.out.println(Name[i]);
-            System.out.println("enter age");
-            Age[i]=s.nextInt();
-            System.out.println(Age[i]);
-        }
+        int n = s.nextInt();
+
+        userDetails userdetails = new userDetails(n);
+        userDetails.enterDetails(n);
+//
+//        int n, age;
+//        String name;
+//        String[] Name = new String[n+1];
+//        int[] Age = new int[n+1];
+//        int i;
+//        for(i=1;i<=n;i++) {
+//            System.out.println("enter name"+i);
+//            String garbageScan1 = s.nextLine();
+//            Name[i]=s.nextLine();
+//            System.out.println(Name[i]);
+//            System.out.println("enter age");
+//            Age[i]=s.nextInt();
+//            System.out.println(Age[i]);
+//        }
 
         //date and time selection
         System.out.println("enter month of entry");
         int month = s.nextInt();
         System.out.println("enter date of entry");
         int date = s.nextInt();
-        System.out.println("enter hours of entry (10-22)");
+        System.out.println("enter hours of entry (10-ss)");
         int hour = s.nextInt();
         // System.out.println("the population @expo2020: "+population.getPopulation(month, date, hour));
 
         System.out.println("which district would you like to enter first? ");
         //use iterator to display districts stored in linkedlists, which is to be initiated in another class
-        district d= new district(); // object created for district class
-        d.dists();
+//        district d= new district(); // object created for district class
+//        d.dists();
+
+
+        distpav dp = new distpav();
+
+        // for showing districts
+        dp.districts();
+
         String garbageScan1 = s.nextLine();
         String selectDist = s.nextLine();
-        String currentDist = selectDist.toLowerCase();
+        // remove lowercases and spaces from the String input
+        String currentDist = selectDist.toLowerCase().replaceAll("\\s+", "");
+//        System.out.println(currentDist);
+
+        parking parkng = new parking(currentDist);
+        String currentParking = parkng.allotPark();
+        System.out.println("your parking is "+currentParking);
+
         System.out.println(currentDist);
+        dp.pavilions(currentDist);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         //call for method in a class to check if currentDist == district
         // and call for function in that function to display the pavilions
 
+        // store parking
+
+        //***************
 
 
 
-        // if district sustainability: parking A-c
-        // if district mobility/jubilee: parking D-F
-        //if district opportunity/ al  forsan: parking g-i
-        // if district sustainability: parking A-c
+
+
+
+
+        // if district sustainability/ al wasl: parking A-c
+        // if district mobility/jubilee/ al wasl: parking D-F
+        //if district opportunity/ al  forsan/ al wasl: parking g-i
 
 
     }
