16d4a864f7fdd04ee580904dfe010e773ba7256a
ysh1th <98638840+ysh1th@users.noreply.github.com>
2022-12-10 17:15:10 +0400

    Create checkPav.java

---
+++ checkPav.java
@@ -0,0 +1,95 @@
+import activities.*;
+
+public class checkPav {
+    public  String pav;
+    public String dist;
+    checkPav(String currentPav, String currentDist) {
+        this.pav = currentPav;
+        this.dist = currentDist;
+    }
+
+    public void allotAct(){
+        switch (this.dist) {
+            case "sustainability":
+                switch (this.pav) {
+                    case "brazil":
+                        // call for another class imported from package
+                        break;
+                    case "Singapore":
+
+                        break;
+                    case "Campus Germany":
+
+                        break;
+                }
+                break;
+//                System.out.println("Or direct to exit?");
+
+            case "opportunity":
+                switch (this.pav) {
+                    case "Saudi Arabia":
+
+                        break;
+                    case "UK":
+
+                        break;
+                    case "China":
+
+                        break;
+
+                }
+
+                break;
+
+
+            case "mobility":
+                switch (this.pav) {
+                    case "South Korea":
+
+                        break;
+                    case "Jamaica":
+
+                        break;
+                    case "France":
+
+                        break;
+                }
+
+                break;
+
+
+
+            case "alforsan":
+                switch (this.pav) {
+                    case "India":
+
+                        break;
+                    case "UAE":
+
+                        break;
+                    case "Japan":
+
+                        break;
+                }
+
+                break;
+
+
+            case "jubilee":
+                switch (this.pav) {
+                    case "Argentina":
+
+                        break;
+                    case "Portugal":
+
+                        break;
+                    case "Canada":
+
+                        break;
+                }
+
+                break;
+        }
+    }
+
+}
