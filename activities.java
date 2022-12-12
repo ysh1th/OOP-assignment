d3d5482ee1925f0ea894d344b0ba45b1f8005cfd
ysh1th <98638840+ysh1th@users.noreply.github.com>
2022-12-12 07:25:19 +0400

    Update activities.java

--- activities.java
+++ activities.java
@@ -1,5 +1,32 @@
 package activities;
 
 public class activities {
+    public class brazilAct(){}
+    
+    public class singaporeAct(){}
+    
+    public class germanyAct(){}
+    
+    public class saudiarabiaAct(){}
+    
+    public class ukcanadaAct(){} 
+    //similar activity for UK and canada 
+
+    public class chinaAct(){}
+
+    public class koreaJapanAct(){}
+
+    public class jamaicaAct(){}
+
+    public class franceAct(){}
+
+    public class indiaAct(){}
+
+    public class uaeAct(){}
+
+    public class argentinaAct(){}
+
+    public class portugalAct(){}
+
 
 }
