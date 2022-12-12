95c79e639f0b3f5b2b1cbde2fe473d26126cc32c
ysh1th <98638840+ysh1th@users.noreply.github.com>
2022-12-12 12:54:08 +0400

    Update asgn.java

--- asgn.java
+++ asgn.java
@@ -9,15 +9,12 @@ import static java.lang.System.exit;
 public class asgn {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("welcome to expos0s0");
-
-        System.out.println("Pls enter number of ppl to enter");
-        int n = s.nextInt();
+        System.out.println("welcome to expo2020");
 
         userDetails userdetails = new userDetails();
-        userDetails.enterDetails();
+        userDetails.enterDetails(); //this will ask for no. of ppl entering and rest all
 
-        System.out.println("which district would you like to enter first? ");
+//        System.out.println("which district would you like to enter first? ");
         //use iterator to display districts stored in linkedlists, which is to be initiated in another class
 //        district d= new district(); // object created for district class
 //        d.dists();
@@ -26,31 +23,10 @@ public class asgn {
         distpav dp = new distpav();
 
         // for getting districts
-        dp.districts(); //change to askdist
-
-        // ********* selects district **********
-//        String garbageScan1 = s.nextLine();
-//        String selectDist = s.nextLine();
-
-        // remove lowercases and spaces from the String input
-//        String currentDist = selectDist.toLowerCase().replaceAll("\\s+", "");
-//        if (currentDist.equals("exit")) {
-//            System.exit(0); //exits code successfully
-//        }
-
-        // ********* allots parking ***********
-//        parking parkng = new parking(currentDist);
-//        String currentParking = parkng.allotPark();
-//        System.out.println("your parking is " + currentParking);
-
-        // shows chosen districts
-//        System.out.println("your chosen district is " + currentDist);
+        dp.askdist(); //change to askdist
 
         System.out.println("choose pavilion to enter\n");
-        dp.pavilions(currentDist);
-        String selectPav = s.nextLine();
-        System.out.println(selectPav);
-        String currentPav = selectPav.toLowerCase().replaceAll("\\s+", "");
+
 
 
     }
