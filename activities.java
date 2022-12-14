6ea55898e9d103d1fe3bbca09eb37531ab1d4ac7
ysh1th <98638840+ysh1th@users.noreply.github.com>
2022-12-14 20:06:07 +0400

    Update activities.java

--- activities.java
+++ activities.java
@@ -1,4 +1,6 @@
 package activities;
+import java.util.Arrays;
+
 
 
 import java.util.*;
@@ -38,11 +40,10 @@ public class activities {
                 System.out.println(b+" grams of CO2 per mile");
                 System.out.println("Hybrid vehicles produce only "+c+" grams of CO2 per mile");
                 System.out.println("Making hybrid vehicles the best choice to go sustainable in this country with high vehicle population!!");
-
             }
         }
-        class SingleInheritance {
-            public void main(String args[]) {
+        class act1 {
+            public void main(String[] args) {
                 B obj = new B(); //derived class object
                 obj.a = 411;
                 obj.b = 15;
@@ -51,9 +52,225 @@ public class activities {
                 obj.show();
             }
         }
-    }
+    }// end of act 1
+
+
+    {// for opportunity pavilions
+        //interface done
+        interface Printable{
+            void print();
+        }
+        interface Showable{
+            void show();
+        }
+        class act2 implements Printable,Showable{
+            public void print(){System.out.println("Hello there! you are in the opportunity zone...");}
+            public void show(){System.out.println("Where you will see countries that aim at\n\tfuture focus\n\tinclusive growth\n\tand youth power\nHave a nice day!");}
+
+            public void main(String[] args){
+                act2 obj = new act2();
+                obj.print();
+                obj.show();
+            }
+        }
+    }// end of act 2
+
 
     {
+        class NegNumExc extends Exception
+        {
+            public NegNumExc ()
+            {
+                super ("This program does not support negative numbers.");
+            }
+        }
+
+        class Prime
+        {
+            public static void main (String[]args)
+            {
+                Scanner sc = new Scanner (System.in);
+
+                int start = sc.nextInt ();
+                int end = sc.nextInt ();
+                try
+                {
+                    if (start < 0 || end <= start)
+                    {
+                        throw new NegNumExc ();
+                    }
+                    else
+                    {
+                        for (int i = start; i <= end; i++)
+                        {
+                            if (isPrime (i))
+                            {
+                                System.out.println (i);
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    System.out.println ("Invalid Inputs.");
+                    return;
+                }
+
+
+            }
+
+            static boolean isPrime (int x)
+            {
+                for (int i = 2; i < (x / 2); i++)
+                {
+                    if (x % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+
+        class InvStudentExc extends Exception
+        {
+            public InvStudentExc ()
+            {
+                super ("Invalid Student.");
+            }
+        }
+
+        class Student
+        {
+            public String name, id;
+            public int age, year;
+
+            Student (String n, int a, int y, String i)
+            {
+                this.name = n;
+                this.id = i;
+
+                this.age = a;
+                this.year = y;
+            }
+        }
+
+
+         class act3{
+            public static void main (String[]args)
+            {
+                Scanner sc = new Scanner (System.in);
+
+                String name, id;
+                int age, year;
+                System.out.println("this pavilion has entry only to registered students");
+                System.out.println("\t!!!registrations are closed!!!\n");
+                System.out.println("To find out if you are allowed...");
+                System.out.println("enter name: ");
+                name = sc.nextLine ();
+                System.out.println("enter id: ");
+                id = sc.nextLine ();
+                System.out.println("enter age: ");
+                age = sc.nextInt ();
+                System.out.println("enter year of study in university: {1,2,3,4}");
+                year = sc.nextInt ();
+
+                Student s = new Student (name, age, year, id);
+                try
+                {
+                    if (isValidName (s.name) && isValidAge (s.age) && isValidYear (s.year)
+                            && isValidId (s.id))
+                    {
+                        System.out.println ("Valid Student.");
+                        System.out.println("But sorry to disappoint you");
+                        System.out.println("this pavilion is temporarily closed for renovation");
+                        System.out.println("but don't worry \nyour OPPORTUNITY awaits you on Oct 1, 2022");
+                        System.out.println("\nSee you at Expo city");
+                    }
+                    else
+                    {
+                        throw new InvStudentExc ();
+                    }
+                }
+                finally
+                {
+                    System.out.println ("Invalid Student.");
+                    return;
+                }
+            }
+            static boolean isValidName (String name)
+            {
+                int i;
+                for (char c:name.toCharArray ())
+                {
+                    i = ((int) c);
+                    if ((i >= 65 && i <= 90) || (i >= 97 && i <= 122))
+                    {
+                        continue;
+                    }
+                    return false;
+                }
+                return true;
+            }
+            static boolean isValidAge (int age)
+            {
+                if (age >= 35 || age <= 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+            static boolean isValidYear (int year)
+            {
+                int digit;
+                while (year != 0)
+                {
+                    digit = year % 10;
+                    year /= 10;
+                    if (digit < 1 || digit > 4)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            static boolean isValidId (String id)
+            {
+                if (id.length () != 6)
+                {
+                    return false;
+                }
+                int char_no;
+                char[] arr = id.toCharArray ();
+                for (int i = 0; i < id.length (); i++)
+                {
+                    char_no = ((int) arr[i]);
+                    if (i == 0)
+                    {
+                        if (arr[i] != 'f')
+                        {
+                            return false;
+                        }
+                    }
+                    else if (i >= 1 && i <= 4)
+                    {
+                        if (i < 48 || i > 57)
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if (arr[i] != 'U')
+                        {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
 
 
     }
